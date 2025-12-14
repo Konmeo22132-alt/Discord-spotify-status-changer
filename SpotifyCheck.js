2ea8f302d5131ab88595c04ae33998f30b125bdb
Konmeo22132-alt <nguyennguyenkhangle@gmail.com>
2025-12-14 20:08:26 +0700

    1

--- SpotifyCheck.js
+++ SpotifyCheck.js
@@ -1,4 +1,4 @@
-const ACCESS_TOKEN = "BQBAIe7keVlgjwL4_g-LxtpUL37wgOHCq2Pq0Nv73evCY9cBnfENVV0asJdnfp3aoFHBfey65FQdst7ENA_4hQA8oX76r4BM2yGSwS_1sDAJSxQFPYHW_mFfV1EtKCkNJuvuNB3zYBZgrDmb0eUey431IfQzKW7aks1o3IIV9ekTES9RnXW1W5epMmbgw8CoDBqylNPMW1Ov-o03KmJsAav5kla4PNPrpxwP-Tcoox_TwTe-o3PSzQoT2g";
+const ACCESS_TOKEN = "";
 
 async function check() {
   const res = await fetch("https://api.spotify.com/v1/me/player", {
@@ -30,3 +30,4 @@ async function check() {
 }
 
 check();
+
